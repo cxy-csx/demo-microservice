651606be7f313b551375b7a786ea8085fa4a072e
czx <1924086038@qq.com>
2023-08-07 20:55:02 +0800

    add swagger document

---
+++ demo-common/src/main/java/com/csx/common/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.csx.common.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableKnife4j
+@EnableSwagger2WebMvc
+public class SwaggerConfig{
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("接口文档")
+                .description("API文档")
+                .contact(new Contact("程序员陈师兄", "https://github.com/cxy-csx/demo", "3041439327@@qq.com"))
+                .version("1.0")
+                .build();
+    }
+
+}
--- demo-service/demo-author/src/main/java/com/csx/author/controller/AuthorController.java
+++ demo-service/demo-author/src/main/java/com/csx/author/controller/AuthorController.java
@@ -3,6 +3,8 @@ package com.csx.author.controller;
 import com.csx.author.service.impl.AuthorServiceImpl;
 import com.csx.model.R;
 import com.csx.model.dtos.LoginDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "登录授权模块")
 public class AuthorController {
 
     @Autowired
