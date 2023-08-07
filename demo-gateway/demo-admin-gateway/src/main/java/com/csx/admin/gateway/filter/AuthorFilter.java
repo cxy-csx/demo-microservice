357094ae5fd7e36a7d88078fb491eece0a903d67
czx <1924086038@qq.com>
2023-08-07 21:47:03 +0800

    add gateway

---
+++ demo-gateway/demo-admin-gateway/src/main/java/com/csx/admin/gateway/filter/AuthorFilter.java
@@ -0,0 +1,45 @@
+package com.csx.admin.gateway.filter;
+
+import com.alibaba.cloud.commons.lang.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.lang.annotation.Annotation;
+
+@Component
+@Slf4j
+public class AuthorFilter implements Ordered, GlobalFilter {
+
+
+
+    /**
+     * 过滤器执行顺序, 值越小, 优先级越高
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 登录认证
+
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+
+
+        log.info("请求对象{}", request);
+
+        // 放行
+        return chain.filter(exchange);
+
+    }
+}
